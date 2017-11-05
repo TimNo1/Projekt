b899180100ceae1035d07705deff44f701c89455
Dario Babojelić <dario.babojelic@fer.hr>
2017-11-05 11:10:04 +0100

    minimizer added2

--- main.cpp
+++ main.cpp
@@ -12,15 +12,15 @@ int main (int argc, char *argv[])
                 Projekt_VERSION_MAJOR,
                 Projekt_VERSION_MINOR);
     
-//    fprintf(stdout, "Enter a string to get all the 3mers of:\n");
-//    std::string s = "";
-//    getline(std::cin, s);
-//
-//    std::vector<finder::kMerTriple*> v = *finder::getKmers(s, 3);
-//
-//    for (long i=0, n=v.size(); i<n; i++) {
-//        fprintf(stdout, "%s %ld\n", v[i]->s.c_str(), v[i]->i);
-//    }
+    fprintf(stdout, "Enter a string to get all the 3mers of:\n");
+    std::string s = "";
+    getline(std::cin, s);
+
+    std::vector<finder::kMerTriple*> v = *finder::getKmers(s, 3);
+
+    for (long i=0, n=v.size(); i<n; i++) {
+        fprintf(stdout, "%s %ld\n", v[i]->s.c_str(), v[i]->i);
+    }
 
     std::unordered_set<minimizer::MinimizerTriple, minimizer::MinimizerTripleHasher> vec = minimizer::computeMinimizers("TAGCATGCGTAAGCG", 4, 3);
     for (const auto& it: vec) {
