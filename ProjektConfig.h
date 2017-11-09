8c6f654a27615451c17122da981be6f693cd9d57
Donik Vrsnak <donik.vrsnak@infinum.hr>
2017-11-09 20:54:00 +0100

    Connects fasta parser as input for minimizers

---
+++ ProjektConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Projekt
+#define Projekt_VERSION_MAJOR 0
+#define Projekt_VERSION_MINOR 1
--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 #include "ProjektConfig.h"
 #include "finder.h"
 #include "minimizer/minimizer.h"
+
+extern "C"{
 #include "fasta.h"
+}
+void printHashFromFastaFile(char* seqfile);
+void simpleExample();
 
 int main (int argc, char *argv[])
 {
@@ -13,6 +18,38 @@ int main (int argc, char *argv[])
                 Projekt_VERSION_MAJOR,
                 Projekt_VERSION_MINOR);
     
+    printHashFromFastaFile(argv[1]);
+    
+    return 0;
+}
+
+void printHashFromFastaFile(char* seqfile){
+    FASTAFILE* ffp = OpenFASTA(seqfile);
+    char* sequenceString;
+    char* sequenceName;
+    int sequenceSize = 0;
+    
+    while (ReadFASTA(ffp, &sequenceString, &sequenceName, &sequenceSize)) {
+        std::cout<< std::string(sequenceName)<<std::endl;
+        std::cout<< std::string(sequenceString, sequenceSize)<<std::endl;
+        
+        std::unordered_set<minimizer::MinimizerTriple> vec = minimizer::computeMinimizers(std::string(sequenceString, sequenceSize), 20, 15);
+        
+        for (const auto& it: vec) {
+            std::cout << "POSITION: " << it.position << "   HASH: " << it.h << "    IS_RC: " << it.rc << std::endl;
+        }
+        
+        std::cout<<std::endl;
+        
+        free(sequenceString);
+        free(sequenceName);
+    }
+    
+    CloseFASTA(ffp);
+}
+
+
+void simpleExample(){
     fprintf(stdout, "Enter a string to get all the 3mers of:\n");
     std::string s = "";
     getline(std::cin, s);
@@ -22,13 +59,4 @@ int main (int argc, char *argv[])
     for (long i=0, n=v.size(); i<n; i++) {
         fprintf(stdout, "%s %ld\n", v[i]->s.c_str(), v[i]->i);
     }
-
-    std::cout << std::endl;
-    std::cout << "minimizer example" << std::endl;
-    std::unordered_set<minimizer::MinimizerTriple> vec = minimizer::computeMinimizers("TAGCATGCGTAAGCG", 4, 3);
-    for (const auto& it: vec) {
-        std::cout << "POSITION: " << it.position << "   HASH: " << it.h << "    IS_RC: " << it.rc << std::endl;
-    }
-    return 0;
 }
-
